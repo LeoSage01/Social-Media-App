23448204d5bc5a86bf16bcf36f0141b0e6f49cbd
Ugwuoke Victor <113137578+LeoSage01@users.noreply.github.com>
2023-11-20 20:24:55 +0100

    Update page.jsx

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -38,7 +38,7 @@ const page = () => {
         return;
       }
 
-      const res = await fetch(`${URL}api/register`, {
+      const res = await fetch(`${URL}/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
