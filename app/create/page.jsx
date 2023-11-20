382cbe1b29e0f6d63432de58e2427f223976fd54
Ugwuoke Victor <113137578+LeoSage01@users.noreply.github.com>
2023-11-21 00:50:55 +0100

    Update page.jsx

--- app/create/page.jsx
+++ app/create/page.jsx
@@ -93,8 +93,8 @@ const page = () => {
 
        <div className="flex justify-center items-center h-screen">
         <div
-          className={`${styles.glass}`}
-          style={{ width: "50%", paddingTop: "1em", maxWidth: "90%, margin: "auto 10px" }}
+          className={`${styles.glass} mx-4`}
+          style={{ width: "50%", paddingTop: "1em" }}
         >
           <div className="title flex flex-col items-center mb-2">
             <h4 className="text-2xl font-bold">What's happening?</h4>
