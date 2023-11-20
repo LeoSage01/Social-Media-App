4ca933beafacea362e2cde4543b25bbd8d0ad575
Ugwuoke Victor <113137578+LeoSage01@users.noreply.github.com>
2023-11-20 21:09:16 +0100

    Update page.jsx

--- app/create/page.jsx
+++ app/create/page.jsx
@@ -94,7 +94,7 @@ const page = () => {
       <div className="flex justify-center px-4 items-center h-screen">
         <div
           className={styles.glass}
-          style={{ width: "45%", paddingTop: "1em" }}
+          style={{ width: "45%", paddingTop: "1em", margin: "auto 10px" }}
         >
           <div className="title flex flex-col items-center mb-2">
             <h4 className="text-2xl font-bold">What's happening?</h4>
