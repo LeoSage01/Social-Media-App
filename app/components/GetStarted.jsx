5d9d542a5179a273b6bb886900fb5ac256f159fe
Ugwuoke Victor <113137578+LeoSage01@users.noreply.github.com>
2023-11-20 20:03:36 +0100

    Update GetStarted.jsx

--- app/components/GetStarted.jsx
+++ app/components/GetStarted.jsx
@@ -12,9 +12,9 @@ const GetStarted = () => {
           className="h-[400px] w-[250px]"
           alt="User image, Saly"
         />
-        <h2 className="text-2xl font-bold mt-4 mb-2 text-center">Explore your creativity</h2>
+        <h2 className="text-2xl font-bold mt-2 sm:mt-4 mb-2 text-center">Explore your creativity</h2>
         <p className="text-center">A tool to explore and share moments with friends</p>
-        <Link className={`${styles.btn} mt-14 sm:mt-8`} href="/login">
+        <Link className={`${styles.btn} mt-8 md:mt-14`} href="/login">
           Get Stared
         </Link>
       </div>
